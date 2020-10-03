f57205c63325dab3b520106767309f489d6388c2
Ashutosh Dubey <idkashutosh@gmail.com>
2020-10-04 00:32:09 +0530

    code refactor

--- src/components/models/ModelSubheader.tsx
+++ src/components/models/ModelSubheader.tsx
@@ -1,25 +1,26 @@
 import React from 'react'
 import Edit from '../../icons/Edit'
-import Save from '../../icons/Save'
+import Trash from '../../icons/Trash'
 
 const ModelSubheader = () => {
     return (
         <div className="subheader py-3 py-lg-8 subheader-transparent" id="kt_subheader">
-            <div className="container-fluid d-flex align-items-center justify-content-between flex-wrap flex-sm-nowrap">
+            <div className="container-fluid d-flex align-items-center justify-content-between flex-wrap flex-sm-nowrap  mx-4">
                 <div className="d-flex align-items-center mr-1">
                     <div className="d-flex align-items-baseline flex-wrap mr-5">
-                        <h2 className="d-flex align-items-center text-dark font-weight-bold my-1 mr-3">Dashboard</h2>
+                        <h2 className="d-flex align-items-center text-dark font-weight-bold my-1 mr-3">Users Table</h2>
                     </div>
                 </div>
                 <div className="d-flex align-items-center flex-wrap">
                     <button className="btn btn-secondary btn-fixed-height font-weight-bold px-2 px-lg-5 mr-2">
+                        <Trash />
+                        <span className="d-none d-md-inline"> Delete Model</span>
+                    </button>
+                    <button className="btn btn-secondary btn-fixed-height font-weight-bold px-2 px-lg-5 mr-2">
                         <Edit />
                         <span className="d-none d-md-inline"> Edit Model</span>
                     </button>
-                    <button className="btn btn-success btn-fixed-height font-weight-bold px-2 px-lg-5 mr-2" disabled>
-                        <Save />
-                        <span className="d-none d-md-inline"> Save Model</span>
-                    </button>
+
                 </div>
             </div>
         </div>
