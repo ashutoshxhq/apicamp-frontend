01cf83aa0a0e484f6ce4c42cc6643fbca3e3168b
Ashutosh Dubey <idkashutosh@gmail.com>
2020-10-30 16:30:48 +0530

    code refactor

--- src/components/models/ModelsOverview.tsx
+++ src/components/models/ModelsOverview.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Download from '../../icons/Download'
+import CloudDownload from '../../icons/CloudDownload'
 
 const ModelsOverview = () => {
     return (
@@ -10,13 +10,13 @@ const ModelsOverview = () => {
                         <div className="container-fluid d-flex align-items-center justify-content-between flex-wrap flex-sm-nowrap">
                             <div className="d-flex align-items-center flex-wrap mr-2">
                                 <h5 className="text-dark font-weight-bold mt-2 mb-2 mr-5">Models</h5>
-                               
+
                             </div>
-                            
+
                             <div className="d-flex align-items-center flex-wrap">
-                                
-                                <button onClick={() => { }} className="btn btn-secondary btn-fixed-height font-weight-bold px-2 px-lg-5 mr-2">
-                                    <Download />
+
+                                <button onClick={() => { }} className="btn btn-primary btn-fixed-height font-weight-bold px-2 px-lg-5 mr-2">
+                                    <CloudDownload />
                                     <span className="d-none d-md-inline"> Source Code</span>
                                 </button>
                             </div>
